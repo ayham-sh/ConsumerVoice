90cd5e116ebbc59ba27dd17d1d5067c8c2db1b9f
ayham alshami <ayham alshami>
2016-08-04 13:16:31 +0300

    signup

--- www/templates/themes/authentication/js/controllers.js
+++ www/templates/themes/authentication/js/controllers.js
@@ -1,4 +1,5 @@
 // Controller of login Page.
+
 appControllers.controller('LoginCtrl', function ($scope, $state, $cordovaOauth, $http, localStorage) {
 
 
@@ -11,9 +12,23 @@ appControllers.controller('LoginCtrl', function ($scope, $state, $cordovaOauth, 
 
 
 // Controller of Signup  Page.
-appControllers.controller('signupCtrl', function ($scope, $state, $cordovaOauth, $http, localStorage) {
+appControllers.controller('signupCtrl', function ($scope, $state, $cordovaOauth, $http) {
 
+	 $scope.user ={
+		name : '',
+		Email : '',
+		Pass1 :'',
+		Pass2 :'',
+		Address :'',
+		Sahip :''
 
+	};
+	$scope.verfity =true;
+  var regex = /^(([^<😠)[\]\\.,;:\s@\"]+(\.[^<😠)[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regex.test($scope.user.Email))
+  {  $scope.verfiy = true  }
+  else
+  {$scope.verfiy = false}
 
 });
 // End of signupCtrl controller.
