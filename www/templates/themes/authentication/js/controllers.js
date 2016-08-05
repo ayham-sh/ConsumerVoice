78b4a22e302da97b4c295cd85651ffc039bf2568
ayham-sh <ayham.mulham772@gmail.com>
2016-08-05 14:39:54 +0300

    Add files via upload

--- www/templates/themes/authentication/js/controllers.js
+++ www/templates/themes/authentication/js/controllers.js
@@ -23,12 +23,34 @@ appControllers.controller('signupCtrl', function ($scope, $state, $cordovaOauth,
 		Sahip :''
 
 	};
+
+ $scope.password = function ( user)
+ {
+   if ( user.Pass1 === user.Pass2)
+   { return "" ; }
+   else {
+     return "the password is not match ";
+   }
+
+ }
+
 	$scope.verfity =true;
   var regex = /^(([^<😠)[\]\\.,;:\s@\"]+(\.[^<😠)[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (regex.test($scope.user.Email))
   {  $scope.verfiy = true  }
   else
   {$scope.verfiy = false}
+
+  $scope.emailvalid= function myfunction( NewForm ) {
+    if(NewForm.EventEmail.$error.email)
+    {
+      return "plaese enter valid Email"
+    }
+    else {
+      return ""
+    }
+  }
+
 
 });
 // End of signupCtrl controller.
